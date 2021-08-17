53b7b72e07f46c894335d8f472f08554207b9055
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2021-08-17 08:44:30 -0700

    Copy Error PIO and Topo tests from geopm/scripts/test
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- service/geopmdpy_test/Makefile.mk
+++ service/geopmdpy_test/Makefile.mk
@@ -32,6 +32,9 @@
 EXTRA_DIST += geopmdpy_test/__main__.py \
               geopmdpy_test/TestPlatformService.py \
               geopmdpy_test/TestDBusXML.py \
+              geopmdpy_test/TestError.py \
+              geopmdpy_test/TestPIO.py \
+              geopmdpy_test/TestTopo.py \
               # end
 
 GEOPMDPY_TESTS = geopmdpy_test/pytest_links/TestDBusXML.test_xml_parse_no_doc \
@@ -71,6 +74,15 @@ GEOPMDPY_TESTS = geopmdpy_test/pytest_links/TestDBusXML.test_xml_parse_no_doc \
                  geopmdpy_test/pytest_links/TestPlatformService.test_write_control \
                  geopmdpy_test/pytest_links/TestPlatformService.test_write_control_invalid \
                  geopmdpy_test/pytest_links/TestPlatformService.test_get_cache \
+                 geopmdpy_test/pytest_links/TestError.test_error_message \
+                 geopmdpy_test/pytest_links/TestPIO.test_domain_name \
+                 geopmdpy_test/pytest_links/TestPIO.test_signal_names \
+                 geopmdpy_test/pytest_links/TestPIO.test_control_names \
+                 geopmdpy_test/pytest_links/TestPIO.test_read_signal \
+                 geopmdpy_test/pytest_links/TestPIO.test_write_control \
+                 geopmdpy_test/pytest_links/TestTopo.test_num_domain \
+                 geopmdpy_test/pytest_links/TestTopo.test_domain_domain_nested \
+                 geopmdpy_test/pytest_links/TestTopo.test_domain_name_type \
                  # end
 
 TESTS = $(GEOPMDPY_TESTS)
